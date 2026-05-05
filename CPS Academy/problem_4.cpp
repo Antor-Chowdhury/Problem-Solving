aa171e027d9d17d65631831db4a0bd30ae073986
Antor-Chowdhury <antor1347@gmail.com>
2026-05-06 00:43:47 +0600

    added problem 4 solution

---
+++ CPS Academy/problem_4.cpp
@@ -0,0 +1,16 @@
+// Platform: CF Group
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int X, Y;
+    cin >> X >> Y;
+
+    cout << X << " + " << Y << " = " << X + Y << "\n";
+    cout << X << " * " << Y << " = " << X * Y << "\n";
+    cout << X << " - " << Y << " = " << X - Y << "\n";
+
+    return 0;
+}
