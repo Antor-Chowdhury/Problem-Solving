3de4abc2e006e9890d362b17f456e617c04d0b65
Antor-Chowdhury <antor1347@gmail.com>
2026-05-06 00:48:12 +0600

    added problem 8 solution

---
+++ CPS Academy/problem_8.cpp
@@ -0,0 +1,13 @@
+// Platform: Marisa OJ
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cin >> a >> b;
+    float result = a / b;
+    printf("%.2f", result);
+    return 0;
+}
